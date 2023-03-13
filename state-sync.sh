fbb1a6896c09cc7f0350869425e75bdab30e7563
freqnik <freqnik@protonmail.com>
2023-03-13 02:56:06 -0400

    State-sync script

---
+++ state-sync.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+SNAP_RPC="https://rpc.sentinel.co:443"
+SNAP_RPC2="https://rpc-sentinel.whispernode.com:443"
+
+LATEST_HEIGHT=$(curl -s $SNAP_RPC/block | jq -r .result.block.header.height); \
+BLOCK_HEIGHT=$((LATEST_HEIGHT - 1000)); \
+TRUST_HASH=$(curl -s "$SNAP_RPC/block?height=$BLOCK_HEIGHT" | jq -r .result.block_id.hash)
+
+sed -i.bak -E "s|^(enable[[:space:]]+=[[:space:]]+).*$|\1true| ; \
+s|^(rpc_servers[[:space:]]+=[[:space:]]+).*$|\1\"$SNAP_RPC,$SNAP_RPC2\"| ; \
+s|^(trust_height[[:space:]]+=[[:space:]]+).*$|\1$BLOCK_HEIGHT| ; \
+s|^(trust_hash[[:space:]]+=[[:space:]]+).*$|\1\"$TRUST_HASH\"|" $HOME/.sentinelhub/config/config.toml
+
+cp $HOME/.sentinelhub/data/priv_validator_state.json $HOME/.sentinelhub/priv_validator_state.json.backup
+
+rm -rf $HOME/.sentinelhub/data
+mkdir $HOME/.sentinelhub/data
+cp $HOME/.sentinelhub/priv_validator_state.json.backup $HOME/.sentinelhub/data/priv_validator_state.json
+
